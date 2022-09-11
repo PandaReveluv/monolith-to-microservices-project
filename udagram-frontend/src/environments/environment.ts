7080061a64ee67d689e1a31161709cc2b8145b24
PandaReveluv <dat19071998@gmail.com>
2022-09-11 16:00:02 +0700

    update frontend url

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ad602ac2b22994129a6089085d44637c-1278508826.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
